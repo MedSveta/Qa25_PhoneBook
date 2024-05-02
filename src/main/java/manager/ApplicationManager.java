e471e96f020e468f3763ec1b643c19efca2ac932
MedSveta <sveta1978medved@gmail.com>
2024-05-02 19:57:31 +0300

    aadd login positive test

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -5,10 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ApplicationManager {
     WebDriver wd;
+    HelperUser helperUser;
 
     public void init() {
         wd = new ChromeDriver();
         wd.navigate().to("https://telranedu.web.app/");
+        helperUser = new HelperUser(wd);
+    }
+
+    public HelperUser getHelperUser() {
+        return helperUser;
     }
 
     public void stop() {
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -2,10 +2,12 @@ package tests;
 
 import org.testng.annotations.Test;
 
-public class LoginTests extends TestBase{
+public class LoginTests extends TestBase {
 
     @Test
-    public  void  test(){
-
+    public void loginSuccess() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("sveta1234@gmail.com", "1234567$Ru");
+        app.getHelperUser().submitLogin();
     }
 }
